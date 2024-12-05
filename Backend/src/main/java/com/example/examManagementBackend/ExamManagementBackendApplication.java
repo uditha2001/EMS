06f93da3d6bb8cd3d008249786e01506ae642836
udithaBro <udithaimesh4@gmail.com>
2024-12-05 23:20:40 +0530

    add created project first time to the github

---
+++ Backend/src/main/java/com/example/examManagementBackend/ExamManagementBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.examManagementBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamManagementBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamManagementBackendApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/example/examManagementBackend/ExamManagementBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.examManagementBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamManagementBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
