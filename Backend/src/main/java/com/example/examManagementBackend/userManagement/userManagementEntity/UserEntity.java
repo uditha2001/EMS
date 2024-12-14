597d2618ac44b757a27ad0b0af3cd121faa8986b
udithaBro <udithaimesh4@gmail.com>
2024-12-15 00:19:58 +0530

    implementing user entity

--- Backend/src/main/java/com/example/examManagementBackend/userManagement/userManagementEntity/UserEntity.java
+++ Backend/src/main/java/com/example/examManagementBackend/userManagement/userManagementEntity/UserEntity.java
@@ -1,10 +1,40 @@
 package com.example.examManagementBackend.userManagement.userManagementEntity;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import java.time.LocalDateTime;
 
+@EntityListeners(AuditingEntityListener.class)
+@Entity
+@Table(name="users")
 public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int userId;
+
+    @Column(nullable = false,unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+    @Column(nullable = false,unique = true)
+    private String email;
+    @Column(nullable = false)
+    private String firstName;
+    @Column(nullable = false)
+    private String lastName;
+    @Column(nullable = false,columnDefinition = "INT DEFAULT 0")
+    private int failed_login_attemps;
+    @Column(nullable = false,columnDefinition = "BOOLEAN DEFAULT 1")
+    private boolean isActive;
+    @CreatedDate
+    @Column(nullable = false,updatable = false)
+    private LocalDateTime createdAt;
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
 
 }
