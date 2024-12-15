062928fa077f0cadc63324cfeceb363162a9516e
udithaBro <udithaimesh4@gmail.com>
2024-12-15 23:19:10 +0530

    initialize basic security

--- Backend/src/main/java/com/example/examManagementBackend/configurations/WebSecuirityConfiguration.java
+++ Backend/src/main/java/com/example/examManagementBackend/configurations/WebSecuirityConfiguration.java
@@ -1,6 +1,6 @@
 package com.example.examManagementBackend.configurations;
 
-import com.example.examManagementBackend.utill.ExceptionHandle.JwtAuthenticationEntryPoint;
+import com.example.examManagementBackend.ExceptionHandle.JwtAuthenticationEntryPoint;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
--- Backend/src/main/java/com/example/examManagementBackend/utill/JwtUtill.java
+++ Backend/src/main/java/com/example/examManagementBackend/utill/JwtUtill.java
@@ -1,4 +1,8 @@
 package com.example.examManagementBackend.utill;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class JwtUtill {
+
 }
