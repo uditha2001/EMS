656621942912c866cf5d1a18cdb82dea47e8c4bd
gimhanadeshan <gimhanadeshan00@gmail.com>
2024-12-17 11:13:48 +0530

    Create UserRoleDTO.java

---
+++ Backend/src/main/java/com/example/examManagementBackend/userManagement/userManagementDTO/UserRoleDTO.java
@@ -0,0 +1,91 @@
+package com.example.examManagementBackend.userManagement.userManagementDTO;
+
+import java.util.List;
+
+public class UserRoleDTO {
+    private Long id; // User ID
+    private String username;
+    private String email;
+    private String firstName;
+    private String lastName;
+    private String password;
+    private List<String> roles; // List of role names assigned to the user
+
+
+    public UserRoleDTO() {}
+
+    public UserRoleDTO(Long id, String username, String email, String firstName, String lastName,String password, List<String> roles) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.password = password;
+        this.roles = roles;
+    }
+
+    public UserRoleDTO(Long id, String username, String email, String firstName, String lastName, List<String> roles) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.roles = roles;
+    }
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public List<String> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(List<String> roles) {
+        this.roles = roles;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+
+}
+
