bc672e1b8dd859bc8dc27e1b1e5c2779c22fbdd1
Aathifmhd <aathifmhd2000@gmail.com>
2024-12-27 23:21:41 +0530

    Button component added

--- Frontend_1/src/pages/Users/Users.tsx
+++ Frontend_1/src/pages/Users/Users.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Button1 from './../../components/Button1'
+import Button2 from './../../components/Button1'
 
 export default function Index() {
   return (
     <div>
-      
+      <Button1 ButtonName={"Create user"}></Button1><br /><br />
+      <Button2 ButtonName={"Create user"}></Button2>
     </div>
   )
 }
