a47b348a1390c58395ad3645e2348aae5f682a30
gimhanadeshan <gimhanadeshan00@gmail.com>
2024-12-29 21:24:48 +0530

    Update RoleDTO.java

--- Backend/src/main/java/com/example/examManagementBackend/userManagement/userManagementDTO/RoleDTO.java
+++ Backend/src/main/java/com/example/examManagementBackend/userManagement/userManagementDTO/RoleDTO.java
@@ -3,14 +3,20 @@ package com.example.examManagementBackend.userManagement.userManagementDTO;
 import java.util.Set;
 
 public class RoleDTO {
+    private Long roleId;
     private String roleName;
     private String description;
     private Set<Long> permissionIds;
 
-    public RoleDTO(String roleName, String roleDescription, Set<Long> permissionIds) {
+    public RoleDTO(Long roleId,String roleName, String roleDescription, Set<Long> permissionIds) {
+        this.roleId = roleId;
         this.roleName = roleName;
         this.description = roleDescription;
         this.permissionIds = permissionIds;
+    }
+
+    public Long getRoleId() {
+        return roleId;
     }
 
     public String getRoleName() {
