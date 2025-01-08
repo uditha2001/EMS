5391276f65984e52863a61d4cf002b230068e016
udithaBro <udithaimesh4@gmail.com>
2025-01-08 20:34:23 +0530

    create required tables

--- Backend/src/main/java/com/example/examManagementBackend/paperWorkflows/entity/CoursesEntity.java
+++ Backend/src/main/java/com/example/examManagementBackend/paperWorkflows/entity/CoursesEntity.java
@@ -1,4 +1,38 @@
 package com.example.examManagementBackend.paperWorkflows.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@EntityListeners(AuditingEntityListener.class)
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class CoursesEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String code;
+    @Column(nullable = false)
+    private String name;
+
+    private String description;
+    @Column(nullable = false)
+    private int semester;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name="degree_program_id",referencedColumnName = "id",nullable = false)
+    private DegreeProgramsEntity degreeProgramsEntity;
 }
--- Backend/src/main/java/com/example/examManagementBackend/paperWorkflows/entity/DegreeProgramsEntity.java
+++ Backend/src/main/java/com/example/examManagementBackend/paperWorkflows/entity/DegreeProgramsEntity.java
@@ -1,4 +1,32 @@
 package com.example.examManagementBackend.paperWorkflows.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@EntityListeners(AuditingEntityListener.class)
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class DegreeProgramsEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String degreeName;
+    private String degreeDescription;
+    @CreatedDate
+    private LocalDateTime createdAt;
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+    @OneToMany(mappedBy = "degreeProgramsEntity",cascade = CascadeType.ALL)
+    private List<CoursesEntity> coursesEntities;
 }
