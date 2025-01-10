832a67780f16a98a939089ffbb14c72266f15296
TimashaWanninyake <160386834+TimashaWanninyake@users.noreply.github.com>
2025-01-10 09:36:36 +0530

    new changes

--- Backend/src/main/java/com/example/examManagementBackend/paperWorkflows/controller/DegreeProgramController.java
+++ Backend/src/main/java/com/example/examManagementBackend/paperWorkflows/controller/DegreeProgramController.java
@@ -1,47 +1,46 @@
-package com.example.examManagementBackend.paperWorkflows.controller;
-
-import com.example.examManagementBackend.paperWorkflows.dto.DegreeProgramDTO;
-import com.example.examManagementBackend.paperWorkflows.entity.DegreeProgramEntity;
-import com.example.examManagementBackend.paperWorkflows.service.DegreeProgramService;
-import jakarta.validation.Valid;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-
-@RestController
-@RequestMapping("/degreeProgram")
-public class DegreeProgramController {
-
-    @Autowired
-    DegreeProgramService degreeProgramService;
-
-    @GetMapping
-    public List<DegreeProgramEntity> getAllDegreePrograms(){
-        return degreeProgramService.getAllDegreePrograms();
-    }
-
-    @PostMapping
-    public ResponseEntity<DegreeProgramEntity> saveDegreeProgram(@Valid @RequestBody DegreeProgramDTO dto){
-        DegreeProgramEntity entity =  degreeProgramService.saveDegreeProgram(dto);
-        return ResponseEntity.ok(entity);
-    }
-
-    @GetMapping("/{id}")
-    public DegreeProgramEntity getOneDegreeProgram(@PathVariable("id") int id){
-        return degreeProgramService.getOneDegreeProgram(id);
-    }
-
-    @PutMapping("/{id}")
-    public DegreeProgramEntity updateDegreeProgram(@Valid @RequestBody DegreeProgramDTO dto, @PathVariable("id") int id){
-        return degreeProgramService.updateDegreeProgram(dto, id);
-    }
-
-    @DeleteMapping("/{id}")
-    public ResponseEntity<String> deleteDegreeProgram(@PathVariable int id){
-        degreeProgramService.deleteDegreeProgram(id);
-        return ResponseEntity.ok("Deleted sucessfully with degreeProgramId: " + id);
-    }
-
-}
+//package com.example.examManagementBackend.paperWorkflows.controller;
+//
+//
+//import com.example.examManagementBackend.paperWorkflows.entity.DegreeProgramsEntity;
+//import jakarta.validation.Valid;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.*;
+//
+//import java.util.List;
+//
+//@RestController
+//@RequestMapping("/degreeProgram")
+//public class DegreeProgramController {
+//
+//    @Autowired
+//    DegreeProgramService degreeProgramService;
+//
+//    @GetMapping
+//    public List<DegreeProgramsEntity> getAllDegreePrograms(){
+//        return degreeProgramService.getAllDegreePrograms();
+//    }
+//
+//    @PostMapping
+//    public ResponseEntity<DegreeProgramsEntity> saveDegreeProgram(@Valid @RequestBody DegreeProgramDTO dto){
+//        DegreeProgramsEntity entity =  degreeProgramService.saveDegreeProgram(dto);
+//        return ResponseEntity.ok(entity);
+//    }
+//
+//    @GetMapping("/{id}")
+//    public DegreeProgramsEntity getOneDegreeProgram(@PathVariable("id") int id){
+//        return degreeProgramService.getOneDegreeProgram(id);
+//    }
+//
+//    @PutMapping("/{id}")
+//    public DegreeProgramsEntity updateDegreeProgram(@Valid @RequestBody DegreeProgramDTO dto, @PathVariable("id") int id){
+//        return degreeProgramService.updateDegreeProgram(dto, id);
+//    }
+//
+//    @DeleteMapping("/{id}")
+//    public ResponseEntity<String> deleteDegreeProgram(@PathVariable int id){
+//        degreeProgramService.deleteDegreeProgram(id);
+//        return ResponseEntity.ok("Deleted sucessfully with degreeProgramId: " + id);
+//    }
+//
+//}
