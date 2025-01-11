27940199ece07bb28df2514a4c22430e6ed78414
udithaBro <udithaimesh4@gmail.com>
2025-01-11 20:18:24 +0530

    create keygenration function in the backend

--- Backend/src/main/java/com/example/examManagementBackend/paperWorkflows/controller/PapersHandelingController.java
+++ Backend/src/main/java/com/example/examManagementBackend/paperWorkflows/controller/PapersHandelingController.java
@@ -1,6 +1,9 @@
 package com.example.examManagementBackend.paperWorkflows.controller;
 
 
+import com.example.examManagementBackend.utill.StandardResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +11,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api/v1/paper")
 public class PapersHandelingController {
 
+    @PostMapping("/generateKeys")
+    public ResponseEntity<StandardResponse> generateKeys() {
+
+        return  null;
+    }
 }
--- Backend/src/main/java/com/example/examManagementBackend/paperWorkflows/service/PapersHandelingService.java
+++ Backend/src/main/java/com/example/examManagementBackend/paperWorkflows/service/PapersHandelingService.java
@@ -1,4 +1,35 @@
 package com.example.examManagementBackend.paperWorkflows.service;
 
+import com.example.examManagementBackend.utill.StandardResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.security.*;
+import java.util.Base64;
+
+@Service
 public class PapersHandelingService {
+
+    public ResponseEntity<StandardResponse> generateKey(){
+        try {
+            KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
+            keyGen.initialize(2048);
+            KeyPair keyPair = keyGen.generateKeyPair();
+            PrivateKey privateKey = keyPair.getPrivate();
+            PublicKey publicKey = keyPair.getPublic();
+            String privateKeyString = Base64.getEncoder().encodeToString(privateKey.getEncoded());
+            String publicKeyString = Base64.getEncoder().encodeToString(publicKey.getEncoded());
+            String[] keys = {privateKeyString, publicKeyString};
+            return new ResponseEntity<StandardResponse>(
+                    new StandardResponse(200,"key generation sucess",keys), HttpStatus.CREATED
+            );
+        }
+        catch(NoSuchAlgorithmException e){
+                return new ResponseEntity<StandardResponse>(
+                        new StandardResponse(500,"key generation failed",null), HttpStatus.INTERNAL_SERVER_ERROR
+                );
+        }
+
+    }
 }
