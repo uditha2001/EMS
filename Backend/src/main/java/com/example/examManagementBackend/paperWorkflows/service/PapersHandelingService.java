550250064a7736507aea6f9ab5911a90db93e09d
uditha <udithaimesh4@gmail.com>
2025-01-11 23:00:25 +0530

    create function to get user name in paperservice

--- Backend/src/main/java/com/example/examManagementBackend/paperWorkflows/service/PapersHandelingService.java
+++ Backend/src/main/java/com/example/examManagementBackend/paperWorkflows/service/PapersHandelingService.java
@@ -1,7 +1,13 @@
 package com.example.examManagementBackend.paperWorkflows.service;
 
+import com.example.examManagementBackend.userManagement.userManagementDTO.LoginResponseDTO;
+import com.example.examManagementBackend.userManagement.userManagementEntity.TokenEntity;
+import com.example.examManagementBackend.userManagement.userManagementEntity.UserEntity;
+import com.example.examManagementBackend.userManagement.userManagementRepo.TokenRepo;
+import com.example.examManagementBackend.utill.JwtUtill;
 import com.example.examManagementBackend.utill.StandardResponse;
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -11,6 +17,11 @@ import java.util.Base64;
 
 @Service
 public class PapersHandelingService {
+    @Autowired
+    TokenRepo tokenRepo;
+    @Autowired
+    JwtUtill jwtUtill;
+
 
     public ResponseEntity<StandardResponse> generateKeys(HttpServletRequest request){
         try {
@@ -31,6 +42,24 @@ public class PapersHandelingService {
                         new StandardResponse(500,"key generation failed",null), HttpStatus.INTERNAL_SERVER_ERROR
                 );
         }
+
+    }
+
+    public String getUserName(HttpServletRequest request){
+        final String authorizationHeader = request.getHeader("Authorization");
+        String acesssToken=null;
+        String userName=null;
+        String refreshToken=null;
+        TokenEntity token=null;
+        LoginResponseDTO loginResponseDTO=null;
+        UserEntity userEntity=null;
+        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+            acesssToken= authorizationHeader.substring(7);
+            token=tokenRepo.findByAcessToken(acesssToken);
+            refreshToken=token.getRefreshToken();
+            userName=jwtUtill.extractUserName(refreshToken);
+        }
+        return userName;
 
     }
 
