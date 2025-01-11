ec739a6076b9aece784a43ed8b7ab8d2aa530a3c
udithaBro <udithaimesh4@gmail.com>
2025-01-11 19:48:49 +0530

    create papperhandler controller

--- Backend/src/main/java/com/example/examManagementBackend/paperWorkflows/controller/PapersHandelingController.java
+++ Backend/src/main/java/com/example/examManagementBackend/paperWorkflows/controller/PapersHandelingController.java
@@ -1,4 +1,11 @@
 package com.example.examManagementBackend.paperWorkflows.controller;
 
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/paper")
 public class PapersHandelingController {
+
 }
