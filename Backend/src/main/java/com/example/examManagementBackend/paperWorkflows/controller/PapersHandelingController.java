b87da66fb222834160539fc8c7fc5cffefa5335c
uditha <udithaimesh4@gmail.com>
2025-01-12 10:29:47 +0530

    create upload file controller

--- Backend/src/main/java/com/example/examManagementBackend/paperWorkflows/controller/PapersHandelingController.java
+++ Backend/src/main/java/com/example/examManagementBackend/paperWorkflows/controller/PapersHandelingController.java
@@ -8,7 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("api/v1/paper")
@@ -23,5 +25,12 @@ public class PapersHandelingController {
     @PostMapping("/generateKeys")
     public ResponseEntity<StandardResponse> generateKeys(HttpServletRequest request) {
         return papersHandelingService.generateKeys(request);
+    }
+
+    @PostMapping("/upload")
+    public ResponseEntity<StandardResponse> upload(@RequestParam("paperFile") MultipartFile paperFile,@RequestParam("fileName") String fileName,HttpServletRequest request) {
+        String OriginalFileName=fileName;
+
+        return null;
     }
 }
