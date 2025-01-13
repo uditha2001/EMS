09c0e453addc8bec2c4dabafa9e07020c2ce4450
gimhanadeshan <gimhanadeshan00@gmail.com>
2025-01-13 14:18:55 +0530

    Create PublicKeyPage.tsx

---
+++ Frontend_1/src/pages/PaperTransfer/PublicKeyPage.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import PublicKey from "./PublicKey";
+
+
+const PublicKeyPage: React.FC = () => {
+  return (
+    <div className="min-h-screen bg-gray-100 p-4">
+      <PublicKey />
+    </div>
+  );
+};
+
+export default PublicKeyPage;
