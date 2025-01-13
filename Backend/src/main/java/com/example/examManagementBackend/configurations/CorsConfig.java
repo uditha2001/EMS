769c1fe85f45f95dc775cf928c6b6da7b3864f1c
uditha <udithaimesh4@gmail.com>
2025-01-13 18:39:10 +0530

    allow all origins

--- Backend/src/main/java/com/example/examManagementBackend/configurations/CorsConfig.java
+++ Backend/src/main/java/com/example/examManagementBackend/configurations/CorsConfig.java
@@ -20,10 +20,10 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins("*")
                         .allowedMethods(GET, POST, PUT, DELETE, OPTIONS)
-                        .allowedHeaders("*")
-                        .allowCredentials(true); // Enable cookies and Authorization headers
+                        .allowedHeaders("*");
+                      //  .allowCredentials(true); // Enable cookies and Authorization headers
             }
         };
     }
