334cb57e2bd5514384877862f2c1f528c2ccfd61
gimhanadeshan <gimhanadeshan00@gmail.com>
2025-01-13 22:27:03 +0530

    Update transferpaper.ts

--- Frontend_1/src/types/transferpaper.ts
+++ Frontend_1/src/types/transferpaper.ts
@@ -2,7 +2,9 @@ export interface EncryptedPaper {
     id: number;
     fileName: string;
     encryptedData: string;
-    sharedAt?: string;  // Optional date when the paper was uploaded
-    fileType?: string;    // Optional field for file type (e.g., 'pdf', 'docx')
+    sharedAt?: string; 
+    fileType?: string; 
+    courseCode?: string;
+    remarks?: string;   
   }
   
