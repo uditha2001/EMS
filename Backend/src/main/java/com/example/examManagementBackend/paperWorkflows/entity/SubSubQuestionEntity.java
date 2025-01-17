ff5769ba6fff458d632225adcccbfd0d2fe81e67
gimhanadeshan <gimhanadeshan00@gmail.com>
2025-01-18 00:09:37 +0530

    Create SubSubQuestionEntity.java

---
+++ Backend/src/main/java/com/example/examManagementBackend/paperWorkflows/entity/SubSubQuestionEntity.java
@@ -0,0 +1,36 @@
+package com.example.examManagementBackend.paperWorkflows.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class SubSubQuestionEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "sub_question_id", nullable = false)
+    private SubQuestionEntity subQuestion;
+
+    private int subSubQuestionNumber;
+
+    private int marks; // Marks for this sub-subquestion
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private QuestionType questionType; // Type of sub-subquestion
+
+    @Column(columnDefinition = "TEXT")
+    private String moderatorComment; // Moderator's comments
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private QuestionModerationStatus status = QuestionModerationStatus.PENDING;
+}
