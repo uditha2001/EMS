624f5ab05956a146138dbc0e5fa74ae380da4edf
gimhanadeshan <gimhanadeshan00@gmail.com>
2025-01-18 22:56:02 +0530

    Update QuestionModerationDTO.java

--- Backend/src/main/java/com/example/examManagementBackend/paperWorkflows/dto/QuestionModerationDTO.java
+++ Backend/src/main/java/com/example/examManagementBackend/paperWorkflows/dto/QuestionModerationDTO.java
@@ -3,9 +3,14 @@ package com.example.examManagementBackend.paperWorkflows.dto;
 import com.example.examManagementBackend.paperWorkflows.entity.QuestionModerationStatus;
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 public class QuestionModerationDTO {
     private Long questionId; // ID of the question (main, sub, or sub-sub)
     private String comment; // Comment by the moderator
     private QuestionModerationStatus status; // Updated status
+    private List<SubQuestionModerationDTO> subQuestions;
 }
+
+
