4ad2963411394ec5a8b281581d622528b88bb4d4
gimhanadeshan <gimhanadeshan00@gmail.com>
2025-01-19 00:43:09 +0530

    Update SubSubQuestionDTO.java

--- Backend/src/main/java/com/example/examManagementBackend/paperWorkflows/dto/SubSubQuestionDTO.java
+++ Backend/src/main/java/com/example/examManagementBackend/paperWorkflows/dto/SubSubQuestionDTO.java
@@ -6,6 +6,7 @@ import lombok.Data;
 
 @Data
 public class SubSubQuestionDTO {
+    private Long subSubQuestionId;
     private int subSubQuestionNumber;
     private QuestionType questionType;
     private float marks;
