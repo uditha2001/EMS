e07620f292fcb0f4db77ac31888e3de2fa1322cc
uditha <udithaimesh4@gmail.com>
2025-01-18 10:17:08 +0530

    implement new methode 'getdataByDegreeId' in courserepo

--- Backend/src/main/java/com/example/examManagementBackend/paperWorkflows/repository/CoursesRepository.java
+++ Backend/src/main/java/com/example/examManagementBackend/paperWorkflows/repository/CoursesRepository.java
@@ -2,10 +2,14 @@ package com.example.examManagementBackend.paperWorkflows.repository;
 
 import com.example.examManagementBackend.paperWorkflows.entity.CoursesEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
 public interface CoursesRepository extends JpaRepository<CoursesEntity, Long> {
     CoursesEntity findBycode(String code);
+    @Query("SELECT ce FROM CoursesEntity ce WHERE ce.degreeProgramsEntity.id= :degreeId")
+    CoursesEntity getdataByDegreeId(@Param("degreeId") Long degreeId);
 }
