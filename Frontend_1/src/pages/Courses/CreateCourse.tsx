bdffa9db28ebc5c01ad5bb5b8e81678d4b7b5563
geethshan <111299707+IroshaGeethanjalie@users.noreply.github.com>
2025-01-18 14:01:33 +0530

    changed the structure of the form

--- Frontend_1/src/pages/Courses/CreateCourse.tsx
+++ Frontend_1/src/pages/Courses/CreateCourse.tsx
@@ -102,35 +102,36 @@ const CreateCourse: React.FC = () => {
           </div>
         </div>
 
-        {/* Semester Selection */}
-        <div>
-          <label className="block text-sm font-medium text-gray-700 mb-1">Semester:</label>
-          <select
-            value={semester}
-            onChange={(e) => setSemester(e.target.value)}
-            className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
-          >
-            <option value="">--Select Semester--</option>
-            <option value="1">Semester 1</option>
-            <option value="2">Semester 2</option>
-          </select>
-        </div>
-
-        {/* Degree Selection */}
-        <div>
-          <label className="block text-sm font-medium text-gray-700 mb-1">Degree:</label>
-          <select
-            value={degree}
-            onChange={(e) => setDegree(e.target.value)}
-            className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
-          >
-            <option value="">--Select Degree--</option>
-            <option value="BCS">BCS</option>
-            <option value="BSC">BSC</option>
-            <option value="BCS.hons">BCS.hons</option>
-            <option value="BSC.hons">BSC.hons</option>
-          </select>
-        </div>
+{/* Semester and Degree Selection */}
+<div className="flex space-x-4">
+    <div className="flex-1">
+      <label className="block text-sm font-medium text-gray-700 mb-1">Semester:</label>
+      <select
+        value={semester}
+        onChange={(e) => setSemester(e.target.value)}
+        className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+      >
+        <option value="">--Select Semester--</option>
+        <option value="1">Semester 1</option>
+        <option value="2">Semester 2</option>
+      </select>
+    </div>
+    <div className="flex-1">
+      <label className="block text-sm font-medium text-gray-700 mb-1">Degree:</label>
+      <select
+        value={degree}
+        onChange={(e) => setDegree(e.target.value)}
+        className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+      >
+        <option value="">--Select Degree--</option>
+        <option value="BCS">BCS</option>
+        <option value="BSC">BSC</option>
+        <option value="BCS.hons">BCS.hons</option>
+        <option value="BSC.hons">BSC.hons</option>
+      </select>
+    </div>
+  </div>
+        
 
         {/* Course Code */}
         <div>
@@ -195,7 +196,7 @@ const CreateCourse: React.FC = () => {
             type="submit"
             className="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 transition"
           >
-            Create
+           Create 
           </button>
           <button
             type="button"
