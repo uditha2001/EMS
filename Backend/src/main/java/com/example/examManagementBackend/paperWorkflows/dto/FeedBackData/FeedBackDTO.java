dbe316edc2a16793d5dd7dc6907ead185468a9a7
uditha <udithaimesh4@gmail.com>
2025-01-21 00:11:56 +0530

    create new dto for get feedback data

--- Backend/src/main/java/com/example/examManagementBackend/paperWorkflows/dto/FeedBackData/FeedBackDTO.java
+++ Backend/src/main/java/com/example/examManagementBackend/paperWorkflows/dto/FeedBackData/FeedBackDTO.java
@@ -3,14 +3,13 @@ package com.example.examManagementBackend.paperWorkflows.dto.FeedBackData;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.aspectj.weaver.patterns.TypePatternQuestions;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class FeedBackDTO {
-    private QuestionObj[] questionObj;
-    private String genaralComment;
+    private questionData[] Question;
+    private String generalComment;
     private String learningOutcomes;
     private String courseContent;
     private String degreeProgram;
