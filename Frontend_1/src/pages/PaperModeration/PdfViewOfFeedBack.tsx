a75437ac9c9355748204fe1c080168dd21928b28
uditha <udithaimesh4@gmail.com>
2025-01-20 20:51:41 +0530

    specify the front end running port

--- Frontend_1/src/pages/PaperModeration/PdfViewOfFeedBack.tsx
+++ Frontend_1/src/pages/PaperModeration/PdfViewOfFeedBack.tsx
@@ -1,11 +1,10 @@
 
-import { useState } from 'react';
 
 type questionData = {
   answer: string;
   comment: string;
   id: number;
-  question: string;
+  question?: string[];
 };
 
 type finalData = {
