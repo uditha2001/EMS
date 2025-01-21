a821cb2019276982efd81f9afa4c2b713c31f699
uditha <udithaimesh4@gmail.com>
2025-01-21 23:15:37 +0530

    create pdf file in backend

--- Backend/src/main/java/com/example/examManagementBackend/paperWorkflows/service/PdfGenrationService.java
+++ Backend/src/main/java/com/example/examManagementBackend/paperWorkflows/service/PdfGenrationService.java
@@ -3,7 +3,6 @@ package com.example.examManagementBackend.paperWorkflows.service;
 import com.example.examManagementBackend.paperWorkflows.dto.FeedBackData.FeedBackDTO;
 import com.example.examManagementBackend.paperWorkflows.dto.FeedBackData.questionData;
 import com.example.examManagementBackend.utill.StandardResponse;
-import com.itextpdf.io.font.PdfEncodings;
 import com.itextpdf.io.font.constants.StandardFonts;
 import com.itextpdf.kernel.colors.DeviceRgb;
 import com.itextpdf.kernel.font.PdfFont;
@@ -12,10 +11,14 @@ import com.itextpdf.kernel.geom.PageSize;
 import com.itextpdf.kernel.pdf.PdfDocument;
 import com.itextpdf.kernel.pdf.PdfWriter;
 import com.itextpdf.layout.Document;
+import com.itextpdf.layout.borders.Border;
 import com.itextpdf.layout.borders.SolidBorder;
 import com.itextpdf.layout.element.Cell;
 import com.itextpdf.layout.element.Paragraph;
 import com.itextpdf.layout.element.Table;
+import com.itextpdf.layout.element.Text;
+import com.itextpdf.layout.properties.TextAlignment;
+import com.itextpdf.layout.properties.VerticalAlignment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
@@ -32,18 +35,22 @@ public class PdfGenrationService {
             PdfWriter writer = new PdfWriter(fileName);
             PdfDocument pdf = new PdfDocument(writer);
             Document document = new Document(pdf, PageSize.A4);
+            float colSize1=190f;
+            float colsize2=250f;
+            float colsize3=100f;
+            float questionTableSize[]={50f,350f,20f,20f,100f};
+            float examineTable[]={270f,270f};
+            float moderatorSign[]={colSize1,colsize2,colsize3};
+            float actionSign[]={180f,180f,180f};
+            float fullColumn[]={540f};
 
             // Add title
-            Paragraph title = new Paragraph("Evaluation Form for Moderation of Examination papers")
+            Paragraph title = new Paragraph("Evaluation Form for Moderation of Examination papers\n" +
+                    "Department of Computer Science-University of Ruhuna")
                     .setBold()
                     .setFontSize(18)
                     .setTextAlignment(com.itextpdf.layout.properties.TextAlignment.CENTER);
-
-            Paragraph subTitle = new Paragraph("Department of Computer Science-University of Ruhuna")
-                    .setFontSize(12)
-                    .setTextAlignment(com.itextpdf.layout.properties.TextAlignment.CENTER);
             document.add(title);
-            document.add(subTitle);
 
             // Add metadata or custom fields as in the form (Degree Program, Course Name, etc.)
             document.add(new Paragraph("Degree Program: " + feedBackDTO.getDegreeProgram()));
@@ -52,7 +59,7 @@ public class PdfGenrationService {
             document.add(new Paragraph("Course Code: " + feedBackDTO.getCourseCode()));
 
             // Table for the main questions
-            Table table = new Table(5); // 5 columns
+            Table table = new Table(questionTableSize); // 5 columns
 
             // Set the table header
             table.addCell(createStyledCell("Row No."));
@@ -91,74 +98,115 @@ public class PdfGenrationService {
                     .setFontSize(14)
                     .setTextAlignment(com.itextpdf.layout.properties.TextAlignment.CENTER));
             commentCell1.setBorder(new SolidBorder(new DeviceRgb(0, 0, 0), 1));
-            commentCell1.setPadding(5);
+            commentCell1.setPadding(10);
+            PdfFont font = PdfFontFactory.createFont(StandardFonts.HELVETICA); // Use Helvetica font
+            commentCell1.setFont(font);
             table.addCell(commentCell1);
-
-            // Create a cell for the general comment
-            Cell CommentCell = new Cell(1, 5); // Spans 5 columns
-            CommentCell.add(new Paragraph(feedBackDTO.getGeneralComment())
-                    .setTextAlignment(com.itextpdf.layout.properties.TextAlignment.LEFT) // Align text to the left
-                    .setMarginLeft(10) // Add a margin for spacing from the left
-                    .setMarginRight(10)); // Add a margin for spacing from the right
-
-// Set border and padding
-            CommentCell.setBorder(new SolidBorder(new DeviceRgb(0, 0, 0), 1)); // Black border with width 1
-            CommentCell.setPaddingLeft(10); // Add padding on the left
-            CommentCell.setPaddingRight(10); // Add padding on the right
-            CommentCell.setPaddingTop(5); // Add padding on the top
-            CommentCell.setPaddingBottom(5); // Add padding on the bottom
             commentCell1.add(new Paragraph(feedBackDTO.getGeneralComment()));
-// Add the cell to the table
-            table.addCell(CommentCell);
             document.add(table);
-
-
-// Names Cell
-            // Create a new table for Name, Signature, and Date
-            float[] columnWidths = {2, 5, 3};
-            Table namesSignatureDateTable = new Table(columnWidths);
-
-// Name Cell
-            Cell namesCell = new Cell(); // Spans 1 column
+            Table table1 = new Table(moderatorSign);
+            Cell namesCell = new Cell();
             namesCell.add(new Paragraph("---------------------")
                     .setTextAlignment(com.itextpdf.layout.properties.TextAlignment.CENTER));
             namesCell.add(new Paragraph("Name")
                     .setBold()
                     .setTextAlignment(com.itextpdf.layout.properties.TextAlignment.CENTER));
-            namesCell.setVerticalAlignment(com.itextpdf.layout.properties.VerticalAlignment.MIDDLE);
-            namesCell.setHeight(50);
-            namesCell.setPadding(5);
-            namesSignatureDateTable.addCell(namesCell);
+            namesCell.setVerticalAlignment(VerticalAlignment.BOTTOM);
+            namesCell.setPadding(10);
+            namesCell.setBorder(Border.NO_BORDER); // Add border for separation
+            table1.addCell(namesCell);
 
 // Signature Cell
-            Cell signatureCell = new Cell(); // Spans 3 columns
+            Cell signatureCell = new Cell();
             signatureCell.add(new Paragraph("---------------------")
                     .setTextAlignment(com.itextpdf.layout.properties.TextAlignment.CENTER));
             signatureCell.add(new Paragraph("Signature")
                     .setBold()
                     .setTextAlignment(com.itextpdf.layout.properties.TextAlignment.CENTER));
-            signatureCell.setVerticalAlignment(com.itextpdf.layout.properties.VerticalAlignment.MIDDLE);
-            signatureCell.setHeight(50);
-            signatureCell.setPadding(5);
-            namesSignatureDateTable.addCell(signatureCell);
+            signatureCell.setVerticalAlignment(VerticalAlignment.BOTTOM);
+            signatureCell.setPadding(10);
+            signatureCell.setBorder(Border.NO_BORDER); // Add border for separation
+            table1.addCell(signatureCell);
 
 // Date Cell
-            Cell dateCell = new Cell(); // Spans 1 column
-            dateCell.add(new Paragraph("---------------------")
+            Cell dateCell = new Cell();
+            dateCell.add(new Paragraph("-------------------")
                     .setTextAlignment(com.itextpdf.layout.properties.TextAlignment.CENTER));
             dateCell.add(new Paragraph("Date")
                     .setBold()
                     .setTextAlignment(com.itextpdf.layout.properties.TextAlignment.CENTER));
-            dateCell.setVerticalAlignment(com.itextpdf.layout.properties.VerticalAlignment.MIDDLE);
-            dateCell.setHeight(50);
-            dateCell.setPadding(5);
-            namesSignatureDateTable.addCell(dateCell);
+            dateCell.setVerticalAlignment(VerticalAlignment.BOTTOM);
+            dateCell.setPadding(10);
+            dateCell.setBorder(Border.NO_BORDER); // Add border for separation
+            table1.addCell(dateCell);
+            document.add(table1.setBorder(new SolidBorder(new DeviceRgb(0, 0, 0), 1)));
+            Table examinerTitle = new Table(examineTable);
+            Cell namesCell1 = new Cell(1,2);
+            namesCell1.add(new Paragraph("Follow Up Action by Examiner/s"))
+                    .setBold()
+                    .setFontSize(14)
+                    .setTextAlignment(com.itextpdf.layout.properties.TextAlignment.CENTER);
+            namesCell1.setBorder(Border.NO_BORDER);
+            namesCell1.setPadding(10);
+            PdfFont examineFont = PdfFontFactory.createFont(StandardFonts.HELVETICA);
+            namesCell1.setFont(examineFont);
+            examinerTitle.addCell(namesCell1);
+            Cell agreeCell = new Cell();
+            agreeCell.add(new Paragraph()
+                    .add(new Text("(a) Agree and Adressed: \n").setBold())
+                    .add(new Text(feedBackDTO.getAgreeAndAddressed()))
+                    )
+                    .setFontSize(12)
+                    .setTextAlignment(TextAlignment.LEFT);
+            agreeCell.setBorder(new SolidBorder(new DeviceRgb(0, 0, 0), 1));
+            agreeCell.setPadding(4);
+           agreeCell.setFont(examineFont);
+           examinerTitle.addCell(agreeCell);
+           Cell disagreeCell = new Cell();
+           disagreeCell.add(new Paragraph()
+                   .add(new Text("(b) Not Agree and Reasson: \n").setBold()).add(new Text("\t"+feedBackDTO.getAgreeAndAddressed()))
 
-            document.add(namesSignatureDateTable);
+                   )
+                    .setFontSize(12)
+                    .setTextAlignment(TextAlignment.LEFT);
+
+            agreeCell.setBorder(new SolidBorder(new DeviceRgb(0, 0, 0), 1));
+           disagreeCell.setPadding(4);
+           disagreeCell.setFont(examineFont);
+           examinerTitle.addCell(disagreeCell);
+           document.add(examinerTitle.setBorder(Border.NO_BORDER));
+           Table examinersTable = new Table(actionSign);
+           examinersTable.addCell(new Cell().add(new Paragraph("Name")).setBorder(Border.NO_BORDER));
+           examinersTable.addCell(new Cell().add(new Paragraph("Signature")).setBorder(Border.NO_BORDER));
+           examinersTable.addCell(new Cell().add(new Paragraph("Date")).setBorder(Border.NO_BORDER));
+           for(int i=0;i<12;i++){
+               examinersTable.addCell(new Cell().add(new Paragraph("---------------------------")).setBorder(Border.NO_BORDER));
+           }
+           document.add(examinersTable.setBorder(new SolidBorder(new DeviceRgb(0, 0, 0), 1)));
+           Table endTable=new Table(fullColumn);
+           Cell learningOutcomeCell = new Cell();
+           learningOutcomeCell.add(new Paragraph( )
+                   .add(new Text("Learning Outcomes:\n").setBold())
+                   .add(feedBackDTO.getLearningOutcomes())
+                   .setFontSize(14)
+                   .setTextAlignment(TextAlignment.LEFT)
+           );
+           endTable.addCell(learningOutcomeCell);
+            Cell CourseContent = new Cell();
+            CourseContent.add(new Paragraph( )
+                    .add(new Text("Course Content:\n").setBold())
+                    .add(feedBackDTO.getCourseContent())
+                    .setFontSize(14)
+                    .setTextAlignment(TextAlignment.LEFT)
+            );
+            endTable.addCell(CourseContent);
+            document.add(endTable);
 
 
-            // Add other details (e.g., comment on marking scheme, action by examiner)
-            document.add(new Paragraph(feedBackDTO.getGeneralComment()));
+
+
+
+
 
             document.close();
             return null;
