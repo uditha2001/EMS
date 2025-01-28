3904ee0e46cfd6c96e1eb2fb6188f551d0492ca1
uditha <udithaimesh4@gmail.com>
2025-01-28 23:56:06 +0530

    add new degree programs card

--- Frontend_1/src/pages/Dashboard/AdminDashboard.tsx
+++ Frontend_1/src/pages/Dashboard/AdminDashboard.tsx
@@ -2,10 +2,11 @@ import useApi from '../../api/api';
 import { useEffect, useState } from 'react';
 import UserRolesDistribution from '../../components/adminDashboardsComponents/UserRolesDistribution';
 import DataCard from '../../components/adminDashboardsComponents/DataCard';
-import { UsersIcon} from '@heroicons/react/24/outline';
+import { UsersIcon,BookOpenIcon,AcademicCapIcon} from '@heroicons/react/24/outline';
 const AdminDashboard = () => {
   const [activeUsers, setActiveUsers] = useState<number>(0);
-  const {getUsersCounts,getActiveUsersCount}=useApi();
+  const {getUsersCounts,getActiveUsersCount,getDegreePrograms}=useApi();
+  const [degreeProgramsCount, setDegreeProgramsCount] = useState<number | string>(0);
   const [userCount, setUserCount] = useState<{ [key: string]: number }>({});
   useEffect(() => {
     getUsersCounts().then(users => {
@@ -20,6 +21,9 @@ const AdminDashboard = () => {
       console.log(activeUsers+"checking");
     }
     );
+    getDegreePrograms().then(degreePrograms => {
+      setDegreeProgramsCount(degreePrograms.data.length);
+    });
 
 }, []);
   return (
@@ -47,17 +51,17 @@ const AdminDashboard = () => {
       <div className='h-40 col-span-1 sm:col-span-2 md:col-span-3 xl:col-span-1'>
         <DataCard
           color="purple"
-          count={activeUsers}
-          title="Active Users"
-          icon={UsersIcon}
+          count={0}
+          title="On Going Examinations"
+          icon={BookOpenIcon}
         />
       </div>
       <div className='h-40 col-span-1 sm:col-span-2 md:col-span-3 xl:col-span-1'>
         <DataCard
-          color="red"
-          count={activeUsers}
-          title="Active Users"
-          icon={UsersIcon}
+          color="pink"
+          count={degreeProgramsCount}
+          title="Total Degree Programs"
+          icon={AcademicCapIcon}
         />
       </div>
     </div>
