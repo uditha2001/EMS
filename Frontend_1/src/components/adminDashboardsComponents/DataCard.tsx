a7c1475ea7efc79529bf007063e756c6eec577c9
uditha <udithaimesh4@gmail.com>
2025-01-28 22:44:56 +0530

    implement custom datacard component

--- Frontend_1/src/components/adminDashboardsComponents/DataCard.tsx
+++ Frontend_1/src/components/adminDashboardsComponents/DataCard.tsx
@@ -9,16 +9,16 @@ type DataCardProps = {
 
 const DataCard: React.FC<DataCardProps> = ({ color, count, title, icon: Icon }) => {
   return (
-    <div className="bg-white p-6 rounded-lg shadow-sm hover:shadow-md transition-shadow duration-200 h-full w-full">
+    <div className={`bg-${color}-500 p-6 rounded-lg shadow-sm hover:shadow-md transition-shadow duration-200 h-full w-full`}>
       <div className="flex items-center justify-between h-full">
         <div>
-          <span className={`text-2xl font-bold text-${color}-600`}>
+          <span className={`text-2xl font-bold text-black`}>
             {count}
           </span>
-          <h3 className="text-gray-500 text-sm mt-1">{title}</h3>
+          <h3 className="text-black text-sm mt-1">{title}</h3>
         </div>
-        <div className={`p-4 rounded-full bg-${color}-100`}>
-          <Icon className={`w-6 h-6 text-${color}-600`} />
+        <div className={`p-4 rounded-full bg-white`}>
+          <Icon className={`w-6 h-6 text-black`}/>
         </div>
       </div>
     </div>
