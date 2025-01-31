d109b53f0a6b2caeaa72ad51f2e036566be1d685
uditha <udithaimesh4@gmail.com>
2025-01-31 19:21:42 +0530

    create exam centers entity

--- Backend/src/main/java/com/example/examManagementBackend/examManagement/entities/ExamCentersEntity.java
+++ Backend/src/main/java/com/example/examManagementBackend/examManagement/entities/ExamCentersEntity.java
@@ -4,11 +4,7 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
-import java.util.Date;
 
 @EntityListeners(AuditingEntityListener.class)
 @Entity
@@ -21,13 +17,10 @@ public class ExamCentersEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String examCenterName;
+    @Column(columnDefinition = "TEXT")
     private String examCenterLocation;
     private Integer examCenterCapacity;
-    @CreatedDate
-    @Column(updatable = false,columnDefinition = "DATETIME",nullable = false)
-    private Date createdAt;
-    @LastModifiedDate
-    @Column(columnDefinition = "DATETIME")
-    private Date updatedAt;
+    private String contactPerson;
+
 
 }
