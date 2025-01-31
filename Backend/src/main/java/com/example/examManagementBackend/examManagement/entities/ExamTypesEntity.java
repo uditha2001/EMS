f1d4fcf7bfdd6f01632bc618964e558b1f48d3f1
uditha <udithaimesh4@gmail.com>
2025-01-31 19:33:16 +0530

    create exam types enum

--- Backend/src/main/java/com/example/examManagementBackend/examManagement/entities/ExamTypesEntity.java
+++ Backend/src/main/java/com/example/examManagementBackend/examManagement/entities/ExamTypesEntity.java
@@ -1,5 +1,6 @@
 package com.example.examManagementBackend.examManagement.entities;
 
+import com.example.examManagementBackend.examManagement.entities.Enums.ExamTypesName;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -14,5 +15,7 @@ public class ExamTypesEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Enumerated(EnumType.STRING)
+    private ExamTypesName name;
 
 }
