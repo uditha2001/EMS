e438505377078533ac6259e286bcc855304ac705
uditha <udithaimesh4@gmail.com>
2025-02-01 17:49:34 +0530

    create project package structure for resultManagement

--- Backend/src/main/java/com/example/examManagementBackend/resultManagement/dto/ResultDTO.java
+++ Backend/src/main/java/com/example/examManagementBackend/resultManagement/dto/ResultDTO.java
@@ -4,10 +4,10 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@AllArgsConstructor
 @NoArgsConstructor
+@AllArgsConstructor
 @Data
 public class ResultDTO {
-
+ private int id;
 
 }
