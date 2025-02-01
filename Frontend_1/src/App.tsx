05c026bfb3ba90f60c4c49a96526662fc84015b3
gimhanadeshan <gimhanadeshan00@gmail.com>
2025-02-02 01:21:59 +0530

    Update App.tsx

--- Frontend_1/src/App.tsx
+++ Frontend_1/src/App.tsx
@@ -331,7 +331,7 @@ function App() {
               />
 
               <Route
-                path="/paper/feedback"
+                path="/paper/feedback/:paperId/:moderatorId"
                 element={renderPage('Feedback | EMS', <Feedback />)}
               />
             </Route>
