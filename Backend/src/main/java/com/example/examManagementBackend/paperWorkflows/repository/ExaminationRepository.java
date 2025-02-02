3161be6299650a58bfd2fd9808eb0b5c9b559654
uditha <udithaimesh4@gmail.com>
2025-02-02 11:46:57 +0530

    create repo methode to get data

--- Backend/src/main/java/com/example/examManagementBackend/paperWorkflows/repository/ExaminationRepository.java
+++ Backend/src/main/java/com/example/examManagementBackend/paperWorkflows/repository/ExaminationRepository.java
@@ -1,8 +1,11 @@
 package com.example.examManagementBackend.paperWorkflows.repository;
 
+import com.example.examManagementBackend.paperWorkflows.entity.CoursesEntity;
 import com.example.examManagementBackend.paperWorkflows.entity.ExaminationEntity;
+import com.example.examManagementBackend.resultManagement.entities.ExamTimeTablesEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -14,4 +17,7 @@ public interface ExaminationRepository extends JpaRepository<ExaminationEntity, 
             "AND (:semester IS NULL OR e.semester = :semester) " +
             "AND (:level IS NULL OR e.level = :level)")
     List<ExaminationEntity> findByFilters(String year, String semester, String level);
+
+    @Query("SELECT e.examTimeTables FROM ExaminationEntity e WHERE e.id= :examinationId")
+    List<ExamTimeTablesEntity> getCoursesUsingExaminationId(@Param("examinationId") Long examinationId);
 }
