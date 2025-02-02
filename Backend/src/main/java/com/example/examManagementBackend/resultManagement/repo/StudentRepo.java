f84e0efcf2f5fe64d71040dc6b38e721a469f069
uditha <udithaimesh4@gmail.com>
2025-02-03 00:08:48 +0530

    create an methode to get students id

--- Backend/src/main/java/com/example/examManagementBackend/resultManagement/repo/StudentRepo.java
+++ Backend/src/main/java/com/example/examManagementBackend/resultManagement/repo/StudentRepo.java
@@ -1,4 +1,15 @@
 package com.example.examManagementBackend.resultManagement.repo;
 
-public interface StudentRepo {
+import com.example.examManagementBackend.resultManagement.entities.StudentsEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@EnableJpaRepositories
+public interface StudentRepo extends JpaRepository<StudentsEntity,Long> {
+        @Query("SELECT s.studentId FROM StudentsEntity s WHERE s.studentNumber= :number")
+        Long getIdBystudentNumber(@Param("number") String studentNumber);
 }
