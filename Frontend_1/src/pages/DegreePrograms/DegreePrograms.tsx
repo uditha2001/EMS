6da0d5a4cb1281e9933281aff1816460a148670d
gimhanadeshan <gimhanadeshan00@gmail.com>
2025-02-04 22:22:14 +0530

    Temporary Removed for bug fixing

--- Frontend_1/src/pages/DegreePrograms/DegreePrograms.tsx
+++ Frontend_1/src/pages/DegreePrograms/DegreePrograms.tsx
@@ -1,20 +1,9 @@
-import DegreeProgramAddCard from "../../components/Cards/DegreeProgramAddCard";
-import DegreeProgramCard from "../../components/Cards/DegreeProgramCard";
 
 
 export default function DegreePrograms() {
   return (
     <div>
-      <div className="flex flex-wrap gap-x-9 gap-y-6">
-      <DegreeProgramCard name="degreeProgram" description="Atlassian uses cookies to improve your browsing experience, perform analytics and research, and conducts"/>
-      <DegreeProgramCard name="degreeProgram" description="Atlassian uses cookies to improve your browsing experience, perform analytics and research, and conducts"/>
-      <DegreeProgramCard name="degreeProgram" description="Atlassian uses cookies to improve your browsing experience, perform analytics and research, and conducts"/>
-      <DegreeProgramCard name="degreeProgram" description="Atlassian uses cookies to improve your browsing experience, perform analytics and research, and conducts"/>
-      <DegreeProgramCard name="degreeProgram" description="Atlassian uses cookies to improve your browsing experience, perform analytics and research, and conducts"/>
-      <DegreeProgramCard name="degreeProgram" description="Atlassian uses cookies to improve your browsing experience, perform analytics and research, and conducts"/>
-      <DegreeProgramCard name="degreeProgram" description="Atlassian uses cookies to improve your browsing experience, perform analytics and research, and conducts"/>
-      <DegreeProgramAddCard/>
-      </div>
+      
     </div>
   )
 }
