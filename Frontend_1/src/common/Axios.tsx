d1d4c26696f807dea7ad050d347df481bdc538a1
uditha <udithaimesh4@gmail.com>
2025-04-01 23:49:21 +0530

    add same network

--- Frontend_1/src/common/Axios.tsx
+++ Frontend_1/src/common/Axios.tsx
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseURL = import.meta.env.VITE_API_URL || "http://localhost:8080/api/v1"
+const baseURL = import.meta.env.VITE_API_URL
 export const Axios= axios.create({
     baseURL: baseURL,
     });
