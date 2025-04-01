406e56a8db26a06c539f7c821751abdec0a64de6
uditha <udithaimesh4@gmail.com>
2025-04-01 23:39:00 +0530

    change base url

--- Frontend_1/src/common/Axios.tsx
+++ Frontend_1/src/common/Axios.tsx
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseURL = 'http://13.53.133.27/api/v1'
+const baseURL = import.meta.env.VITE_API_URL || "http://localhost:8080/api/v1"
 export const Axios= axios.create({
     baseURL: baseURL,
     });
