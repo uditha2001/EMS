32a0757d647584476c9bf823b7acc3b6319261ee
uditha <udithaimesh4@gmail.com>
2025-04-03 16:18:10 +0530

    change origin

--- Frontend_1/src/common/Axios.tsx
+++ Frontend_1/src/common/Axios.tsx
@@ -1,6 +1,4 @@
 import axios from 'axios'
-console.log("VITE_API_URL: ", import.meta.env.VITE_API_URL);
-console.log("VITE_MODE: ", import.meta.env.MODE);  // Check the current mode
 const baseURL = import.meta.env.VITE_API_URL ;
 export const Axios= axios.create({
     baseURL: baseURL,
