367e2c13ca09305e0cbc1777734c43e3daa7262e
gimhanadeshan <gimhanadeshan00@gmail.com>
2025-04-08 18:15:40 +0530

    Update ExamTypesForm.tsx

--- Frontend_1/src/pages/ExamTypes/ExamTypesForm.tsx
+++ Frontend_1/src/pages/ExamTypes/ExamTypesForm.tsx
@@ -73,7 +73,7 @@ export default function ExamTypesForm({
               type="submit"
               className="flex justify-center rounded bg-primary py-2 px-6 font-medium text-gray hover:bg-opacity-90"
             >
-              {editId !== null ? 'Update Exam Type' : 'Add Exam Type'}
+              {editId !== null ? 'Update' : 'Add'}
             </button>
           </div>
         </div>
